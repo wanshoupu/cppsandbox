ae88dcfcc0fd39f23fc244165454e4e91ccf9a9a
Shoupu Wan <swan@roblox.com>
2023-04-03 22:58:12 -0700

    implement zigzag conversion (to be finished)

---
+++ leetcode/zigzag_tests.cpp
@@ -0,0 +1,88 @@
+#include <gtest/gtest.h>
+
+// The string "PAYPALISHIRING" is written in a zigzag pattern on a given number
+// of rows like this: (you may want to display this pattern in a fixed font for
+// better legibility)
+//
+// P   A   H   N
+// A P L S I I G
+// Y   I   R
+// And then read line by line: "PAHNAPLSIIGYIR"
+//
+// Write the code that will take a string and make this conversion given a
+// number of rows:
+//
+// string convert(string s, int numRows);
+//
+//
+// Example 1:
+//
+// Input: s = "PAYPALISHIRING", numRows = 3
+// Output: "PAHNAPLSIIGYIR"
+// Example 2:
+//
+// Input: s = "PAYPALISHIRING", numRows = 4
+// Output: "PINALSIGYAHRPI"
+// Explanation:
+// P     I    N
+// A   L S  I G
+// Y A   H R
+// P     I
+// Example 3:
+//
+// Input: s = "A", numRows = 1
+// Output: "A"
+#include <string>
+#include <algorithm>
+
+namespace zigzag {
+    class Solution {
+    public:
+        std::string convert(std::string, int);
+
+    private:
+        int row(int i, int n);
+    };
+
+    std::string Solution::convert(std::string s, int numRows) {
+        // transformation ith letter to (x,y)
+        // (i, n) --> (x, y)
+        // transformation (x, y) letter to j
+        // (x, y, n) --> j
+        std::vector<std::vector<char>> mat(numRows, std::vector<char>());
+        for (int i = 0; i < s.size(); ++i) {
+            int c = row(i, numRows);
+            mat.at(c).push_back(s.at(i));
+        }
+        std::vector<char> concat;
+        for (const auto &vec: mat) {
+            concat.insert(concat.end(), vec.begin(), vec.end());
+        }
+        return {concat.begin(), concat.end()};
+    }
+
+    // TODO
+    int Solution::row(int i, int n) {
+        auto mod = i % (n + 1);
+        if (mod == n) {
+            return 0;
+        }
+        return 0;
+    }
+
+} // namespace zigzag
+
+TEST(Zigzag, HappyCase3) {
+    zigzag::Solution sol;
+    EXPECT_EQ(sol.convert("PAYPALISHIRING", 3), "PAHNAPLSIIGYIR");
+}
+
+TEST(Zigzag, HappyCase4) {
+    zigzag::Solution sol;
+    EXPECT_EQ(sol.convert("PAYPALISHIRING", 4), "PAHNAPLSIIGYIR");
+}
+
+TEST(Zigzag, EdgeCase) {
+    zigzag::Solution sol;
+    EXPECT_EQ(sol.convert("A", 1), "A");
+}
